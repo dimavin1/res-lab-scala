4ed58f1d338d65628d5487b8e8218e8a4c6a9f90
dimavin <dimavin@gmail.com>
2021-03-28 18:31:30 -0400

    Move new line from before to after line.

--- app/src/main/scala/irld/y1970/GeoParser1970.scala
+++ app/src/main/scala/irld/y1970/GeoParser1970.scala
@@ -3,7 +3,6 @@ package irld.y1970
 import better.files._
 
 import scala.collection.mutable
-import scala.collection.mutable.TreeSet
 
 object GeoParser1970 {
   val YEAR = "1970"
@@ -12,34 +11,34 @@ object GeoParser1970 {
     "([A-Z][a-zA-Z&.,'!;()/ -]{3,})(?i)(,\\sInc.)?(?i)(,\\sIncorporated)?[,.]?\\s[A-Z]?[0-9 l]+(\\.\\d{1,3})?".r
   private[y1970] val SubRgx = ".*\\.\\d{1,3}.*".r
 
-  @throws[Exception]
-  def main(args: Array[String]): Unit = try {
-    val inFile: File = File(Dir, "1970_geo_index.txt")
-    val outFile: File = File(Dir, "1970_comp.txt")
-    val outFile2: File = File(Dir, "1970_parents.txt")
-    val outFile3: File = File(Dir, "1970_distinct_parents.txt")
-    val compSet: mutable.TreeSet[String] = mutable.TreeSet[String]()
+  def main(args: Array[String]): Unit = {
+    val geoIndex = File(Dir, "1970_geo_index.txt")
+    val companies = File(Dir, "1970_comp.txt")
+    val parents = File(Dir, "1970_parents.txt")
+    val distinctParents = File(Dir, "1970_distinct_parents.txt")
+    val compSet = mutable.TreeSet[String]()
 
-    outFile.overwrite("")
-    outFile2.overwrite("")
-    outFile3.overwrite("")
-    val lines = inFile lines DefaultCharset
+    companies overwrite ""
+    parents overwrite ""
+    distinctParents overwrite ""
+
+    val lines = geoIndex lines DefaultCharset
     lines foreach {
-      printCompanies(outFile, outFile2, compSet, _)
+      saveCompanies(companies, parents, compSet, _)
     }
-    compSet.foreach {
-      outFile3.appendLine().append(_)
+    compSet foreach {
+      distinctParents appendLine _
     }
   }
 
-  def printCompanies(outFile: File, outFile2: File, compSet: mutable.TreeSet[String], s: String): Unit = {
+  def saveCompanies(companies: File, parents: File, compSet: mutable.TreeSet[String], s: String): Unit = {
     val ms = CompanyRgx.findAllMatchIn(s)
     for (m <- ms) {
       val company = m.group(0)
       println(company)
-      outFile.appendLine().append(company)
+      companies.appendLine(company)
       if (!SubRgx.matches(company)) {
-        outFile2.appendLine().append(company)
+        parents.appendLine(company)
         compSet += company
       }
     }
