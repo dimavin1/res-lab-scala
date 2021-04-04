48b54ec2184e310346e1833951d20ee04fa52884
dimavin <dimavin@gmail.com>
2021-04-04 16:09:56 -0400

    1979 parents manual start codes.

--- app/src/main/scala/irld/y1979/GeoCodes1979.scala
+++ app/src/main/scala/irld/y1979/GeoCodes1979.scala
@@ -7,8 +7,8 @@ object GeoCodes1979 {
   def main(args: Array[String]): Unit = {
     val Year = "1979"
     val Dir = "./app/src/main/resources/y" + Year
-    val DivisionRgx = "(?i)division".r
-    val CompanyCodeRgx = """[A-Z].*?\b(([A-Z][0-9liI]+?)|([0-9l]{2,}))([,.][0-9lI]{0,3})?\b""".r
+    val CodeRgx = " .{1,6}$".r
+    val CompanyCodeRgx = """(.*)( .{1,6})$""".r
     val DecimalRgx = """[.,].{1,3}$""".r
 
     val in = File(Dir, Year + "-geo-parents-manual.txt")
@@ -20,7 +20,9 @@ object GeoCodes1979 {
     val lines = in.lines(DefaultCharset).toList
 
     lines.
-      filter(DivisionRgx.unanchored.matches(_)).
-      map { l => geoDistinct.appendLine(l) }
+      filter(CodeRgx.unanchored.matches(_)).
+      map { l =>
+
+        geoDistinct.appendLine(l) }
   }
 }
